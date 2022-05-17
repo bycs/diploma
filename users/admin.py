6e606c72a4cbc3b66e57dd1dd0d898f06e878d23
Stas Cartoon <bycs@mail.ru>
2022-05-17 22:05:56 +0300

    Изменение моделей users

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-from .forms import CustomUserChangeForm, CustomUserCreationForm
-from .models import CustomUser
+from users.forms import CustomUserChangeForm, CustomUserCreationForm
+from users.models import CustomUser, Role, RoleUser
 
 
 class CustomUserAdmin(UserAdmin):
@@ -15,4 +16,20 @@ class CustomUserAdmin(UserAdmin):
     ]
 
 
+class RoleAdmin(admin.ModelAdmin):
+    model = Role
+    list_display = ["role_name"]
+    search_fields = ["role_name"]
+
+
+class RoleUserAdmin(admin.ModelAdmin):
+    model = RoleUser
+    list_display = ["user_id", "role_id"]
+    list_filter = ["role_id"]
+    search_fields = ["user_id", "role_id"]
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Role, RoleAdmin)
+admin.site.register(RoleUser, RoleUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,17 +1,59 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=30, null=True, verbose_name="Имя")
+    middle_name = models.CharField(max_length=30, null=True, verbose_name="Отчество")
+    last_name = models.CharField(max_length=30, null=True, verbose_name="Фамилия")
+    is_verification = models.BooleanField(default=False, null=False, verbose_name="Подтвержден?")
+
+    @property
+    def fullname(self):
+        if self.middle_name is None:
+            return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
+
+    def __repr__(self):
+        return f"<User {self.username}: {self.fullname}>"
 
 
 class Role(models.Model):
     id = models.BigAutoField(primary_key=True)
-    role_name = models.CharField(max_length=30, unique=True, null=False)
+    role_name = models.CharField(max_length=20, unique=True, null=False, verbose_name="Имя роли пользователя")
+
+    class Meta:
+        verbose_name = "Список ролей"
+        verbose_name_plural = "Список ролей"
+
+    def __str__(self):
+        return self.role_name
+
+    def __repr__(self):
+        return f"<Role {self.role_name} id={self.id}"
 
 
 class RoleUser(models.Model):
     id = models.BigAutoField(primary_key=True)
-    role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Пользователь")
+    role_id = models.ForeignKey(Role, on_delete=models.CASCADE, verbose_name="Роль")
+
+    class Meta:
+        verbose_name = "Роль пользователя"
+        verbose_name_plural = "Роли пользователей"
+
+    def __str__(self):
+        return self.id
+
+    def __repr__(self):
+        return f"<RoleUser {self.role_id} для {self.user_id}"
