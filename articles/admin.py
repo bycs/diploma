ae78f44c4feb6766992926c6a8d233265d9d1861
Stas Cartoon <bycs@mail.ru>
2022-05-20 00:01:35 +0300

    fix admin articles

--- articles/admin.py
+++ articles/admin.py
@@ -5,17 +5,17 @@ from django.contrib import admin
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
-    list_display = ["title", "is_show", "author", "updated"]
-    list_filter = ["author", "is_show", "updated", "created"]
-    search_fields = ["title", "text"]
+    list_display = ("title", "is_show", "author", "updated")
+    list_filter = ("author", "is_show", "updated", "created")
+    search_fields = ("title", "text")
     ordering = ("-updated",)
     filter_horizontal = ("category",)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     model = Category
-    list_display = ["category_name"]
-    search_fields = ["category_name"]
+    list_display = ("category_name",)
+    search_fields = ("category_name",)
     filter_horizontal = ("group_user",)
 
 
