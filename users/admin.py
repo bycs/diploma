403dcbceb6fccb0f0f07d74d0d02e2ac8d1b6269
Stas Cartoon <bycs@mail.ru>
2022-05-19 23:39:31 +0300

    Оптимизация модели пользователя

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,44 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from users.forms import CustomUserChangeForm, CustomUserCreationForm
 from users.models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["email", "username", "fullname", "birthday"]
-    ordering = ("email",)
+    list_display = ("username", "email", "full_name", "birthday", "is_verification")
+    ordering = ("is_verification", "email")
+    fieldsets = (
+        (None, {"fields": ("username", "email")}),
+        (
+            "ФИО",
+            {
+                "fields": (
+                    "first_name",
+                    "middle_name",
+                    "last_name",
+                )
+            },
+        ),
+        ("Разрешения", {"fields": ("is_staff", "is_active", "is_verification")}),
+        ("Личные данные", {"fields": ("birthday",)}),
+        ("Полномочия", {"fields": ("groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {"fields": ("username", "email")}),
+        (
+            "ФИО",
+            {
+                "fields": (
+                    "first_name",
+                    "middle_name",
+                    "last_name",
+                )
+            },
+        ),
+        ("Разрешения", {"fields": ("is_staff", "is_active", "is_verification")}),
+        ("Личные данные", {"fields": ("birthday",)}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -10,20 +10,22 @@ class CustomUser(AbstractUser):
     middle_name = models.CharField(max_length=30, blank=True, null=True, verbose_name="Отчество")
     last_name = models.CharField(max_length=30, null=True, verbose_name="Фамилия")
     birthday = models.DateField(null=True, verbose_name="День рождения")
-    is_verification = models.BooleanField(default=False, null=False, verbose_name="Подтвержден?")
+    is_verification = models.BooleanField(default=False, null=False, verbose_name="Проверен")
 
     @property
-    def fullname(self):
+    def full_name(self):
         if self.middle_name is None:
             return f"{self.first_name} {self.last_name}"
         return f"{self.first_name} {self.middle_name} {self.last_name}"
+
+    full_name.fget.short_description = "ФИО"
 
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
 
     def __str__(self):
-        return self.username
+        return self.full_name
 
     def __repr__(self):
-        return f"<User {self.username}: {self.fullname}>"
+        return f"<User {self.username}: {self.full_name}>"
